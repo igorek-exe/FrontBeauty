0c3b45256d9f78967e0f63f603a9e420f471f899
Andrew <starandry83@mail.ru>
2025-10-10 19:37:07 +0300

    change chore

---
+++ src/components/forms/MasterExperienceForm/index.model.ts
@@ -0,0 +1,1 @@
+export { useMasterExperienceForm } from './model/useMasterExperienceForm';
--- src/components/forms/MasterExperienceForm/index.tsx
+++ src/components/forms/MasterExperienceForm/index.tsx
@@ -1,5 +1,4 @@
 export { MasterExperienceForm } from './UI/MasterExperienceForm';
-export { useMasterExperienceForm } from './model/useMasterExperienceForm';
 export type {
     MasterExperienceFormProps,
     ExperienceItem,
